e4bc8b1f850ba8286cd5c05c6459da6d11fd6c20
Viljami S <viljami.salminen@gmail.com>
2013-02-12 20:54:17 +0200

    Comment the js

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,11 +1,22 @@
+// Variables
 var hasTouch = false,
   ua = navigator.userAgent,
   docEl = document.documentElement;
 
-// JavaScript support
+
+// "Detect" JavaScript support
 docEl.className = docEl.className.replace(/(^|\s)no-js preload(\s|$)/, " js ");
 
-// Font-face support
+
+// Detect "font-face" support
+//
+// p.s. this doesn't check support on desktop browsers
+// so you should use Modernizr in combination with this
+// UA detection to get the most out of it.
+// BUT: in many cases this is detection is all that's needed.
+//
+// Can I use: http://caniuse.com/fontface
+//
 var isFontfaceSupported = (function () {
   if (ua.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Nokia)|(Opera (Mini|Mobi))|(w(eb)?OSBrowser)|(UCWEB)|(Windows Phone OS 7)|(XBLWP)|(ZuneWP)/)) {
     return false;
@@ -13,11 +24,14 @@ var isFontfaceSupported = (function () {
   return true;
 })();
 
+
+// Was "font-face" supported?
 if (isFontfaceSupported) {
   docEl.className = docEl.className.replace(/(^|\s)no-fontface(\s|$)/, " fontface ");
 }
 
-// Touch support
+
+// Detect "touch" support and act accordingly
 if (("ontouchstart" in window) || window.DocumentTouch && document instanceof DocumentTouch || ua.match(/(Windows Phone OS)/)) {
   docEl.className = docEl.className.replace(/(^|\s)no-touch(\s|$)/, " touch ");
   hasTouch = true;
