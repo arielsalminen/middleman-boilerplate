155767581b816594bc2c1188f705a96ba71bd581
Viljami Salminen <viljami.salminen@gmail.com>
2013-02-12 17:02:58 +0200

    Progressive enhancement for font-face detection

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -6,15 +6,23 @@ var hasTouch = false,
   docEl = document.documentElement;
 
 // Remove "preload" and "no-js" classes from <html> element and add "js" class
-docElement.className = docElement.className.replace(/(^|\s)no-js preload(\s|$)/, 'js ');
+docEl.className = docEl.className.replace(/(^|\s)no-js preload(\s|$)/, " js ");
+
+// Detect fontface support
+var isFontfaceSupported = (function () {
+  if (ua.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Nokia)|(OSRE\/)|(Opera (Mini|Mobi))|(w(eb)?OSBrowser)|(UCWEB)|(Windows Phone OS 7)|(XBLWP)|(ZuneWP)/)) {
+    return false;
+  }
+  return true;
+})();
+
+// Remove "no-fontface" and "fontface" class for <html> element
+if (isFontfaceSupported) {
+  docEl.className = docEl.className.replace(/(^|\s)no-fontface(\s|$)/, " fontface ");
+}
 
 // Check for touch support and add "touch" class for <html> element
 if (("ontouchstart" in window) || window.DocumentTouch && document instanceof DocumentTouch || ua.match(/(Windows Phone OS)/)) {
-  docEl.className += " touch";
+  docEl.className += " touch ";
   hasTouch = true;
-}
-
-// Check for font-face support and add "no-fontface" class for <html> element
-if (ua.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Nokia)|(OSRE\/)|(Opera (Mini|Mobi))|(w(eb)?OSBrowser)|(UCWEB)|(Windows Phone OS 7)|(XBLWP)|(ZuneWP)/)) {
-  docEl.className += " no-fontface";
 }
