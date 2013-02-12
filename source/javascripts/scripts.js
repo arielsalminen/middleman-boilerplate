49132e31d2b27b0ebe7c217b0e022b52a8019473
Viljami S <viljami.salminen@gmail.com>
2013-02-12 19:47:17 +0200

    Fix comments

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,14 +1,11 @@
-//= require_tree .
-
-// Variables
 var hasTouch = false,
   ua = navigator.userAgent,
   docEl = document.documentElement;
 
-// Remove "preload" and "no-js" classes from <html> element and add "js" class
+// JavaScript support
 docEl.className = docEl.className.replace(/(^|\s)no-js preload(\s|$)/, " js ");
 
-// Detect fontface support
+// Font-face support
 var isFontfaceSupported = (function () {
   if (ua.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Nokia)|(Opera (Mini|Mobi))|(w(eb)?OSBrowser)|(UCWEB)|(Windows Phone OS 7)|(XBLWP)|(ZuneWP)/)) {
     return false;
@@ -16,12 +13,11 @@ var isFontfaceSupported = (function () {
   return true;
 })();
 
-// Remove "no-fontface" and "fontface" class for <html> element
 if (isFontfaceSupported) {
   docEl.className = docEl.className.replace(/(^|\s)no-fontface(\s|$)/, " fontface ");
 }
 
-// Check for touch support and add "touch" class for <html> element
+// Touch support
 if (("ontouchstart" in window) || window.DocumentTouch && document instanceof DocumentTouch || ua.match(/(Windows Phone OS)/)) {
   docEl.className = docEl.className.replace(/(^|\s)no-touch(\s|$)/, " touch ");
   hasTouch = true;
