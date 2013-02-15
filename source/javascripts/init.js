75509ee446a1ab78b2bf9ad31e5b01b37a71b6a3
Viljami Salminen <viljami.salminen@gmail.com>
2013-02-15 10:03:23 +0200

    Cleaning up init.js

--- source/javascripts/init.js
+++ source/javascripts/init.js
@@ -1,15 +1,12 @@
+
 // Variables
 var hasTouch = false,
   ua = navigator.userAgent,
   doc = document,
   docEl = doc.documentElement;
 
-
-
 // "Detect" JavaScript support
 docEl.className = docEl.className.replace(/(^|\s)no-js preload(\s|$)/, " js ");
-
-
 
 // A fix is on the way to get Windows Phone 8 to recognize
 // CSS pixels rather than device pixels (which is preferred behavior).
@@ -22,8 +19,6 @@ if (ua.match(/IEMobile\/10\.0/)) {
   msViewportStyle.appendChild(doc.createTextNode("@-ms-viewport{width:auto!important}"));
   doc.getElementsByTagName("head")[0].appendChild(msViewportStyle);
 }
-
-
 
 // Detect "font-face" support
 //
@@ -39,12 +34,11 @@ var isFontfaceSupported = (function () {
   }
   return true;
 })();
+
 // Was "font-face" supported?
 if (isFontfaceSupported) {
   docEl.className = docEl.className.replace(/(^|\s)no-fontface(\s|$)/, " fontface ");
 }
-
-
 
 // Detect "touch" support and act accordingly
 //
@@ -54,22 +48,4 @@ if (isFontfaceSupported) {
 if (("ontouchstart" in window) || window.DocumentTouch && document instanceof DocumentTouch || ua.match(/(Windows Phone OS)/)) {
   docEl.className = docEl.className.replace(/(^|\s)no-touch(\s|$)/, " touch ");
   hasTouch = true;
-}
-
-
-
-// Detect input type=file support
-var isFileInputSupported = (function () {
-  // Handle devices which falsely report support
-  if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
-    return false;
-  }
-  // Create test element
-  var el = document.createElement("input");
-  el.type = "file";
-  return !el.disabled;
-})();
-// Was "file input" supported?
-if (isFileInputSupported) {
-  docEl.className = docEl.className.replace(/(^|\s)no-fileinput(\s|$)/, " fileinput ");
 }
