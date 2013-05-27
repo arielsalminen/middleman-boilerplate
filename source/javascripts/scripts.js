682d59fea79bca57961ff341d068feb457bdc9e6
Viljami Salminen <viljami.salminen@gmail.com>
2013-05-27 13:47:23 +0300

    Adds some example JS code

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -2,6 +2,17 @@
 if ("querySelector" in document && 
     "localStorage" in window && 
     "addEventListener" in window) {
+  
+  // Alternatively, instead of on.js, just
+  // var $ = document.querySelectorAll.bind(document);
+  // Element.prototype.on = Element.prototype.addEventListener;
+  // $("somelink")[0].on('touchstart', handleTouch);
+  
+  // Custom events:
+  // $('a').on('foo', function () {
+  //    foo was fired
+  // });
+  // $('a:first-child').trigger('foo');
 
   // Enquire usage
   enquire.register("screen and (min-width: 30em)", {
