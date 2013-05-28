b83ed12ea4a868b910bbec3a17d50869f93b8794
Viljami Salminen <viljami.salminen@gmail.com>
2013-05-28 09:23:37 +0300

    Remove on.js

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -2,31 +2,15 @@
 if ("querySelector" in document && 
     "localStorage" in window && 
     "addEventListener" in window) {
-  
-  // Alternatively, instead of on.js, just
-  // var $ = document.querySelectorAll.bind(document);
-  // Element.prototype.on = Element.prototype.addEventListener;
+
   // $("somelink")[0].on('touchstart', handleTouch);
-  
-  // Custom events:
-  // $('a').on('foo', function () {
-  //    foo was fired
-  // });
-  // $('a:first-child').trigger('foo');
+  var $ = document.querySelectorAll.bind(document);
+  Element.prototype.on = Element.prototype.addEventListener;
 
   // Enquire usage
   enquire.register("screen and (min-width: 30em)", {
     match : function() {},
     unmatch : function() {}
-  });
-
-  // Looping through links
-  $(".selector a").forEach(function(el, i) {
-    i++;
-    el.on("click", function(event) {
-      event.preventDefault();
-      // Something happens
-    });
   });
 
 }
