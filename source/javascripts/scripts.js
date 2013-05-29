7feef5a1ffc114f85a4b831b87c2be5fe7478ff6
Viljami Salminen <viljami.salminen@gmail.com>
2013-05-29 16:07:42 +0300

    Adds comments

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,11 +1,20 @@
+//
 // Bootstrap the JavaScript application
+//
+// JS in this file is visible only to modern browsers
+// after the first if statement. You can also use classes
+// "no-modern" & "modern" which are added in init.js
+//
+// Author: @viljamis
+//
 if ("querySelector" in document && 
     "localStorage" in window && 
     "addEventListener" in window) {
 
   // $("somelink")[0].on('touchstart', handleTouch);
-  var $ = document.querySelectorAll.bind(document);
-  Element.prototype.on = Element.prototype.addEventListener;
+  var doc = document,
+    $ = doc.querySelectorAll.bind(doc);
+    Element.prototype.on = Element.prototype.addEventListener;
 
   // Enquire usage
   enquire.register("screen and (min-width: 30em)", {
